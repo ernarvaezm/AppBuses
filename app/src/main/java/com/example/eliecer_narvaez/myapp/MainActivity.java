427056613130c7192c168f8a866b9d15819f9327
Jose Elias Benavides Varela <Jose Elias Benavides Varela>
2016-11-21 23:04:22 -0600

    Some images

--- app/src/main/java/com/example/eliecer_narvaez/myapp/MainActivity.java
+++ app/src/main/java/com/example/eliecer_narvaez/myapp/MainActivity.java
@@ -79,12 +79,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_myBus) {
             startActivity(new Intent(MainActivity.this, MyBusActivity.class));
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
